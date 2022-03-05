9e251f6f8b348c6d3a2f9e079f37b1f867c371d9
nkdram <nkdramkum@gmail.com>
2022-03-05 14:31:38 +0100

    Send email check conditions

---
+++ src/Foundation/RulesEngine/rendering/Rules/SendEmailSubscriptionRule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mvp.Foundation.RulesEngine.Rules
+{
+    /// <summary>
+    /// Sitecore Send Email subscription check
+    /// </summary>
+    public class SendEmailSubscriptionRule : Rule
+    {
+        public override bool Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
