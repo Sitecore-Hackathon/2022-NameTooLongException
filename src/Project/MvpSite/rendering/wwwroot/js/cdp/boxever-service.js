d09c6552e4efde8ca74b81d05f458c0189f21231
nkdram <nkdramkum@gmail.com>
2022-03-05 19:20:43 +0100

    Trackview moved below body

--- src/Project/MvpSite/rendering/wwwroot/js/cdp/boxever-service.js
+++ src/Project/MvpSite/rendering/wwwroot/js/cdp/boxever-service.js
@@ -1,7 +1,7 @@
-﻿//NAMETOOLONGEXCEPTION--CUSTOM CODE :: Boxever view page events
+﻿//NAMETOOLONGEXCEPTION--CUSTOM CODE :: Boxever init tracker
 var boxeverService = {};
 (function () {
-    this._config = {
+    window.boxeverService._config = {
         "channel": "WEB",
         "language": "EN",
         "currency": "EUR",
@@ -9,42 +9,26 @@ var boxeverService = {};
         "clientKey": "psfu6uh05hsr9c34rptlr06dn864cqrx",
         "domain": "mvp.sc-mvp.localhost"
     }
-    // Define the Boxever queue
-    var boxeverq = boxeverq || [];
-    window._boxeverq = boxeverq;
+   
 
     ///Initialize Service
     this.init = function () {
         // Define the Boxever queue
+        var boxeverq = boxeverq || [];
+        window._boxeverq = boxeverq;
         
         // Define the Boxever settings
         _boxever_settings = {
-            client_key: this._config.clientKey, // Replace with your client key
+            client_key: window.boxeverService._config.clientKey, // Replace with your client key
             target: 'https://api.boxever.com/v1.2', // Replace with your API target endpoint specific to your data center region
-            cookie_domain: this._config.domain // Replace with the top level cookie domain of the website that is being integrated e.g ".example.com" and not "www.example.com"
+            cookie_domain: window.boxeverService._config.domain // Replace with the top level cookie domain of the website that is being integrated e.g ".example.com" and not "www.example.com"
         };
         // Import the Boxever library asynchronously
         (function () {
             var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.id = "cdpSettings";
             s.src = 'https://d1mj578wat5n4o.cloudfront.net/boxever-1.4.8.min.js';
             var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
-        })();
-        window._boxeverq.push(function () {
-            var viewEvent = {
-                "channel": this._config.channel,
-                "type": "VIEW",
-                "language": this._config.language,
-                "currency": this._config.currency,
-                "page": window.location.pathname,
-                "pos": this._config.pos,
-                "browser_id": window.Boxever.getID()
-            };
-            // Invoke event create
-            // (<event msg>, <callback function>, <format>)
-            console.log(viewEvent);
-            window.Boxever.eventCreate(viewEvent, function (data) { }, 'json');
-        });
+        })();        
     };
-
     this.init();
 }).apply(boxeverService);
---
+++ src/Project/MvpSite/rendering/wwwroot/js/cdp/boxever-trackview.js
@@ -0,0 +1,28 @@
+﻿//NAMETOOLONGEXCEPTION--CUSTOM CODE :: Boxever Tracking View
+$(document).ready(function () {
+    function delayUntilBoxeverIsReady() {
+        if (window.Boxever && window.Boxever.getID() !== 'anonymous' && window._boxeverq) {
+            window._boxeverq.push(function () {
+                var viewEvent = {
+                    "channel": window.boxeverService._config.channel,
+                    "type": "VIEW",
+                    "language": window.boxeverService._config.language,
+                    "currency": window.boxeverService._config.currency,
+                    "page": window.location.pathname,
+                    "pos": window.boxeverService._config.pos,
+                    "browser_id": window.Boxever.getID()
+                };
+                // Invoke event create
+                // (<event msg>, <callback function>, <format>)
+                console.log(viewEvent);
+                window.Boxever.eventCreate(viewEvent, function (data) { }, 'json');
+            });
+        } else {
+            const timeToWaitInMilliseconds = 1000;
+            console.log(`Boxever is not ready yet. Waiting ${timeToWaitInMilliseconds}ms before retrying.`);
+            window.setTimeout(delayUntilBoxeverIsReady, timeToWaitInMilliseconds);
+        }
+    }
+    delayUntilBoxeverIsReady();
+
+});
